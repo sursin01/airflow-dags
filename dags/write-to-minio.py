543ae12ed5e70f0c9f8070264722d7198c4e4146
sursin01 <suraj.singh@xenonstack.com>
2025-06-12 18:22:10 +0530

    dags

--- dags/write-to-minio.py
+++ dags/write-to-minio.py
@@ -1,7 +1,6 @@
 from airflow import DAG
-from airflow.contrib.operators.spark_submit_operator import  SparkSubmitOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime
-
 default_args = {
     'start_date': datetime(2024, 1, 1),
     'retries': 1,
